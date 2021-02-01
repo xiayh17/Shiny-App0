bdada2fa1e8b4a3894572db8350d59db015d06ef
xiayh17 <xiayh17@gmail.com>
2021-02-01 12:26:13 +0800

    "helper.R"

---
+++ helper.R
@@ -0,0 +1,16 @@
+deg = function(exprSet,design,contrast.matrix){
+  ##step1
+  fit <- lmFit(exprSet,design)
+  ##step2
+  fit2 <- contrasts.fit(fit, contrast.matrix)
+  ##这一步很重要，大家可以自行看看效果
+
+  fit2 <- eBayes(fit2)  ## default no trend !!!
+  ##eBayes() with trend=TRUE
+  ##step3
+  tempOutput = topTable(fit2, coef=1, n=Inf)
+  nrDEG = na.omit(tempOutput)
+  #write.csv(nrDEG2,"limma_notrend.results.csv",quote = F)
+  head(nrDEG)
+  return(nrDEG)
+}
